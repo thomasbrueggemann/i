4e21c7c91e70cdb769f41dfda54c3607718f3cce
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-01-17 20:53:02 +0100

    store iot data

--- ich.js
+++ ich.js
@@ -1,1 +1,33 @@
 var restify = require("restify");
+var MongoClient = require("mongodb").MongoClient;
+
+// create server
+var server = restify.createServer({
+  name: "Ich",
+});
+server.use(restify.bodyParser());
+
+// ROUTES
+server.post("/", function create(req, res, next) {
+
+	// insert data into mongodb
+	console.log(req.body);
+	global.spacetime.insertMany(req.body);
+
+	res.send(200, {
+		"ack": true,
+		"err": null
+	});
+	return next();
+});
+
+// open mongodb connection
+MongoClient.connect("mongodb://localhost:27017/ich", function(err, db) {
+	console.log("Connected correctly to mongodb server");
+
+	// collections
+	global.spacetime = db.collection("spacetime");
+
+	// start restify server
+	server.listen(18080);
+});
