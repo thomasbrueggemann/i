aaf1270dd0d959361e5e8c0d316991f8e8684dec
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-01-19 13:25:27 +0100

    return latest entry

--- ich.js
+++ ich.js
@@ -2,39 +2,48 @@ var restify = require("restify");
 var MongoClient = require("mongodb").MongoClient;
 
 // create server
-var server = restify.createServer({
-  name: "Ich",
-});
+var server = restify.createServer({ name: "Ich" });
 server.use(restify.bodyParser());
 
-// ROUTES
+// POST /
 server.post("/", function create(req, res, next) {
+    // prepare time data
+    var data = req.body.map(function(item) {
+        item["time"] = new Date(item["time"]);
+        return item;
+    });
 
-	// prepare time data
-	var data = req.body.map(function(item) {
+    // insert data into mongodb
+    global.spacetime.insertMany(data);
 
-		item["time"] = new Date(item["time"]);
-		return item;
-	});
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(200, { ack: true, err: null });
+    return next();
+});
 
-	// insert data into mongodb
-	global.spacetime.insertMany(data);
+// GET /
+server.get("/", function(req, res, next) {
+    // find latest entry and return it
+    global.spacetime.findOne({}, { sort: { time: -1 } }, function(err, data) {
+        // handle error
+        if (err) {
+            res.send(500, err);
+        }
+        // return data else {
+            res.send(200, data);
+        }
 
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.send(200, {
-		"ack": true,
-		"err": null
-	});
-	return next();
+        return next();
+    });
 });
 
 // open mongodb connection
 MongoClient.connect("mongodb://localhost:27017/ich", function(err, db) {
-	console.log("Connected correctly to mongodb server");
+    console.log("Connected correctly to mongodb server");
 
-	// collections
-	global.spacetime = db.collection("spacetime");
+    // collections
+    global.spacetime = db.collection("spacetime");
 
-	// start restify server
-	server.listen(18080);
+    // start restify server
+    server.listen(18080);
 });
